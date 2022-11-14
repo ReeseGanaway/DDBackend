37972f6d5026ad1a834ed85cdbfaedb75988d0e6
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2022-11-14 13:18:03 -0500

    Getting ready for heroku

--- src/main/java/com/ganaway/backend/security/SecurityConfig.java
+++ src/main/java/com/ganaway/backend/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/api/v1/login/**").permitAll();
         http.authorizeRequests().antMatchers("/api/v1/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/v1/**").hasAnyAuthority("USER");
         http.authorizeRequests().antMatchers(POST, "/api/v1/user/save/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
