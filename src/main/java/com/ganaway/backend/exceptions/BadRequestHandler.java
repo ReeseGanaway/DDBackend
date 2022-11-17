13294d67006ba53dd5a11cf03ec05663db0d32ae
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2022-11-16 19:04:19 -0500

    Working on login custom exception

--- src/main/java/com/ganaway/backend/exceptions/BadRequestHandler.java
+++ src/main/java/com/ganaway/backend/exceptions/BadRequestHandler.java
@@ -1,5 +1,6 @@
 package com.ganaway.backend.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class BadRequestHandler {
 
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     public ErrorBean handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.info("Here");
         ErrorBean errorBean = new ErrorBean();
         errorBean.setMessage(e.getMessage());
         return errorBean;
